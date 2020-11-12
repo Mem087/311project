f152f6ecb3bc0e314d6382c566ccbdc2945d5933
Bizmarky <memccallum98@gmail.com>
2020-11-11 20:53:21 -0500

    Bare bones Autocorrect class

---
+++ Autocorrect.py
@@ -0,0 +1,18 @@
+class Autocorrect:
+
+    def __init__(self, fileName):
+        #NOT FINISHED
+        print("Set up init function")
+        
+
+    def createDict(self, fileName):
+        #NOT FINISHED
+        print("Set up createDict function")
+
+    def searchDict(self, word):
+        #NOT FINISHED
+        print("Set up searchDict function")
+
+    def run(self, word):
+        #NOT FINISHED
+        print("Set up run function to return suggestions based on word")
