c1aee1e9f6dc03713356e3eb1a25a41897ce26ba
Hannah Shin <hs033@bucknell.edu>
2020-11-15 15:36:27 -0500

    created the init and createDict function and added our word list txt file

--- Autocorrect.py
+++ Autocorrect.py
@@ -1,18 +1,21 @@
 class Autocorrect:
 
     def __init__(self, fileName):
-        #NOT FINISHED
-        print("Set up init function")
-        
+        self.file = fileName
+        self.dict = {'a': [], 'b': [], 'c': [], 'd': [], 'e': [], 'f': [], 'g': [], 'h': [], 'i': [], 'j': [], 'k': [], 'l': [], 'm': [
+        ], 'n': [], 'o': [], 'p': [], 'q': [], 'r': [], 's': [], 't': [], 'u': [], 'v': [], 'w': [], 'x': [], 'y': [], 'z': []}
 
-    def createDict(self, fileName):
-        #NOT FINISHED
-        print("Set up createDict function")
+    def createDict(self):
+        fp = open('allwords.txt')
+        word = fp.readline()
+        while word:
+            self.dict[word[0].lower()].append(word[:-1])
+            word = fp.readline()
 
     def searchDict(self, word):
-        #NOT FINISHED
+        # NOT FINISHED
         print("Set up searchDict function")
 
     def run(self, word):
-        #NOT FINISHED
+        # NOT FINISHED
         print("Set up run function to return suggestions based on word")
