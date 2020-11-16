3ba6f995c429eecaa9b686ca74f5a6a9cd67b47e
Hannah Shin <hs033@bucknell.edu>
2020-11-15 20:15:41 -0500

    finished fucntions

--- Autocorrect.py
+++ Autocorrect.py
@@ -6,16 +6,62 @@ class Autocorrect:
         ], 'n': [], 'o': [], 'p': [], 'q': [], 'r': [], 's': [], 't': [], 'u': [], 'v': [], 'w': [], 'x': [], 'y': [], 'z': []}
 
     def createDict(self):
-        fp = open('allwords.txt')
+        fp = open(self.file)
         word = fp.readline()
         while word:
             self.dict[word[0].lower()].append(word[:-1])
             word = fp.readline()
 
-    def searchDict(self, word):
-        # NOT FINISHED
-        print("Set up searchDict function")
+    def longestCommonSub(self, word1, word2):
+        c = []
+        b = []
+        # created c and b matrix of length word1 as the num rows and word2 length be num columns
+        for x in range(len(word1)):
+            c.append([])
+            b.append([])
+            for y in range(len(word2)):
+                c[x].append(0)
+                b[x].append(0)
 
-    def run(self, word):
-        # NOT FINISHED
-        print("Set up run function to return suggestions based on word")
+        for i in range(len(word1)):
+            for j in range(len(word2)):
+                if word1[i] == word2[j]:
+                    c[i][j] = 1 + c[i-1][j-1]
+                    b[i][j] = b[i-1][j-1]
+                elif c[i-1][j] >= c[i][j-1]:
+                    c[i][j] = c[i-1][j]
+                    b[i][j] = b[i-1][j]
+                else:
+                    c[i][j] = c[i][j-1]
+                    b[i][j] = b[i][j-1]
+        return c[len(word1)-1][len(word2)-1]
+
+    def searchDict(self, word):
+        bestWord = None
+        bestScore = 0
+        for i in range(len(self.dict[word[0].lower()])):
+            score = self.longestCommonSub(word, self.dict[word[0].lower()][i])
+            if score > bestScore:
+                bestWord = self.dict[word[0].lower()][i]
+                bestScore = score
+        return bestWord
+
+    def run(self):
+        self.createDict()
+        while(1):
+            word = input("Enter a word: ")
+            print(auto.searchDict(word))
+            contin = input("Continue? yes or no ")
+            if contin.lower() == 'no' or contin.lower() == 'n':
+                break
+
+
+auto = Autocorrect('allwords.txt')
+auto.run()
+# auto.createDict()
+# while(1):
+#     word = input("Enter a word: ")
+#     print(auto.searchDict(word))
+#     contin = input("Continue? yes or no ")
+#     if contin.lower() == 'no' or contin.lower() == 'n':
+#         break
